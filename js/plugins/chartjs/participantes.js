75ce9ad7e30fe890c4074189ff728c9647dd25ba
icksonb <ickson94@gmail.com>
2017-07-10 16:08:42 -0300

    Grafico participantes

---
+++ js/plugins/chartjs/participantes.js
@@ -0,0 +1,26 @@
+//Sampel Pie Doughnut Chart
+var PieDoughnutChartSampleData = [
+    {
+        value: 100,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+]
+
+ window.onload = function() {
+
+
+  window.DoughnutChartSample = new Chart(document.getElementById("doughnut-chart-sample").getContext("2d")).Pie(PieDoughnutChartSampleData,{
+   responsive:true
+  });
+  
+
+ };
+ 
