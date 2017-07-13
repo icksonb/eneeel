fb45dadc989c16bd564e44226f38abfbeeac7078
icksonb <ickson94@gmail.com>
2017-07-13 18:04:37 -0300

    Atualização dos gráficos por Ajax.

--- js/plugins/chartjs/participantes.js
+++ js/plugins/chartjs/participantes.js
@@ -15,11 +15,29 @@ var PieDoughnutChartSampleData = [
 ]
 
  window.onload = function() {
+ 	var part;
 
+	$.ajax({
+		url: "http://www.eneeel.com/aplicativo/numeroParticipantes.php", 
+		type: "POST",
+		//headers: {'X-Requested-With': 'XMLHttpRequest'},
+		crossDomain: true,
+        //dataType: 'jsonp',
+		success: function(data) 
+		{
+				console.log(data);
+				part = data.split(",");
+				PieDoughnutChartSampleData[0].value = part[0];
+				PieDoughnutChartSampleData[1].value = part[1];
 
-  window.DoughnutChartSample = new Chart(document.getElementById("doughnut-chart-sample").getContext("2d")).Pie(PieDoughnutChartSampleData,{
+				window.DoughnutChartSample = new Chart(document.getElementById("doughnut-chart-sample").getContext("2d")).Pie(PieDoughnutChartSampleData,{
    responsive:true
   });
+			
+		}
+	});
+
+  
   
 
  };
